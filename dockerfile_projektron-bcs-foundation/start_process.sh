6cae0575acc15e73f0aa6bdebba2403186acba14
Quantentunnel <2110463+Quantentunnel@users.noreply.github.com>
2024-06-02 10:53:42 +0200

    Bugfix
    Bugfix. Container was restarted again and again due to errors.

--- dockerfile_projektron-bcs-foundation/start_process.sh
+++ dockerfile_projektron-bcs-foundation/start_process.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+LOCK_FILE="/tmp/start_processes.lock"
+
+# Prüfen, ob das Skript schon einmal ausgeführt wurde
+if [ -f "$LOCK_FILE" ]; then
+    echo "Script has already been executed. Skipping."
+    exit 0
+fi
+
+# Lock-Datei erstellen
+touch "$LOCK_FILE"
+
 # Starte den ursprünglichen Prozess im Hintergrund
-if [ -z "$(ls -A /opt/projektron/bcs)" ] && [ -z "$(ls -A /opt/projektron/bcs/tomcat)" ]; then
-	echo "Directories are empty, running background process."
-	tail -f /dev/null &
+if [ -z "$(ls -A /opt/projektron/bcs)" ] || [ -z "$(ls -A /opt/projektron/bcs/tomcat)" ]; then
+    echo "Directories are empty, running background process."
+    tail -f /dev/null
 else
-	echo "Directories are not empty, starting the startup script."
-	/opt/projektron/bcs/tomcat/bin/startup.sh &
-	sleep 600
-	echo "Starting Projectron.sh after 10 minutes."
-	/opt/projektron/bcs/bin/Projectron.sh start
+    echo "Directories are not empty, starting the startup script."
+    # Starte den ursprünglichen Prozess
+    /opt/projektron/bcs/tomcat/bin/startup.sh
+    tail -f /dev/null
 fi
